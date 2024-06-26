a1ee0066a1709525ed2d3dcad23b356f7ba30aa9
anth <zyle1104@gmail.com>
2024-06-26 17:53:27 +0900

    📦️ Add: packages install
    기본 패키지 인스톨

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.KAFKA,
+  //   options: {
+  //     client: {
+  //       clientId: 'nestjs',
+  //       brokers: ['localhost:9092'],
+  //     },
+  //     consumer: {
+  //       groupId: 'nestjs-consumer',
+  //     },
+  //   },
+  // });
+
   await app.listen(3000);
 }
 bootstrap();
